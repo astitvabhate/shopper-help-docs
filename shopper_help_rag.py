2b0ca5dfca9e9a3caadbd198316ab4bd09e7e4aa
astitvabhate <bhateastitva204@gmail.com>
2025-09-05 01:43:34 +0530

    Update app.py and requirements.txt

--- shopper_help_rag.py
+++ shopper_help_rag.py
@@ -31,7 +31,7 @@ class StreamHandler(BaseCallbackHandler):
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:8000", "http://localhost:8000"],
+    allow_origins=["http://127.0.0.1:8000", "http://localhost:8000","https://astitvabhate.github.io"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
